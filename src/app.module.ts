1443b293a5c17099c1a9ebb42e34bbde56f8caef
Sam Bulatov <stylesam@yandex.ru>
2019-07-06 14:09:21 +0300

    dev: #time 10m Добавлены переменные окружения для запуска на heroku

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,13 +6,13 @@ import { TracksModule } from './tracks/tracks.module'
 import { SharedModule } from './shared/shared.module'
 import { StatesModule } from './states/states.module'
 
-import { env } from '../env'
+import { DATABASE_URL, env } from '../env'
 import { PassportModule } from '@nestjs/passport'
 
 @Module({
   imports: [
     TypegooseModule.forRoot(
-      `mongodb://${env.database.host}:${env.database.port.toString()}/${env.database.name}`,
+      DATABASE_URL,
       {
         useNewUrlParser: true,
         useCreateIndex: true,
